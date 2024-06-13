73a1a12bae7dc56037d25e08f952e883bef00061
izzylppz <https://github.com/Shmizzy>
2024-06-13 15:10:36 -0500

    init

--- crm.js
+++ crm.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var promptSync = require("prompt-sync");
-/* import * as mysql from "mysql2"; */
 var mysql = require("mysql2");
 /* import { RowDataPacket } from "mysql2"; */
 var connection = mysql.createConnection({
@@ -10,6 +9,7 @@ var connection = mysql.createConnection({
     password: 'Deeboo49@',
     database: 'crm_database'
 });
+connection.connect();
 /* //CREATE
 connection.query('INSERT INTO employees (name, age, position) VALUES (?,?,?)', ['david', 42, 'baker']);
 connection.commit()
@@ -23,16 +23,18 @@ var menu = function () {
     console.log('4.Delete employees');
     console.log('5.EXIT');
     var userInput = prompt('Enter your response: ');
-    return userInput;
+    var input = parseInt(userInput);
+    console.clear();
+    return input;
 };
-/*  console.log('Welcome to CRM!');
+console.log('Welcome to CRM!');
 console.log('1.View employees');
 console.log('2.Create employee');
 console.log('3.Update employee');
 console.log('4.Delete employees');
 console.log('5.EXIT');
-const userInput = prompt('Enter your response: ');
-const input = parseInt(userInput);  */
+var userInput = prompt('Enter your response: ');
+var input = parseInt(userInput);
 var view = function () {
     connection.query('SELECT * FROM employees', function (err, result) {
         if (err)
@@ -59,7 +61,7 @@ var update = function () {
     var newPosition = prompt('What is the employees new position?');
     connection.query('UPDATE employees SET name = ?, age = ?, position = ? WHERE id = ? ', [newName, parseInt(newAge), newPosition, parseInt(employeeId)], function (err, result) {
         if (err)
-            throw (err);
+            console.log(err);
         console.log(result);
     });
     connection.commit();
@@ -72,17 +74,17 @@ var deleteEmp = function () {
         console.log(result);
     });
 };
-connection.connect();
-var userInput;
-do {
-    userInput = menu();
-    if (userInput === '1')
+while (true) {
+    if (input === 1)
         view();
-    if (userInput === '2')
+    if (input === 2)
         create();
-    if (userInput === '3')
+    if (input === 3)
         update();
-    if (userInput === '4')
+    if (input === 4)
         deleteEmp();
-} while (userInput !== '5');
+    if (input > 4)
+        break;
+    break;
+}
 connection.end();
--- crm.ts
+++ crm.ts
@@ -1,7 +1,7 @@
 import * as promptSync from 'prompt-sync';
-/* import * as mysql from "mysql2"; */
 import * as mysql from "mysql2";
 /* import { RowDataPacket } from "mysql2"; */
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -9,13 +9,8 @@ const connection = mysql.createConnection({
     password: 'Deeboo49@',
     database: 'crm_database'
 });
+connection.connect();
 
-interface Employee {
-    id: number
-    name: string
-    age: number
-    position: string
-}
 
 /* //CREATE
 connection.query('INSERT INTO employees (name, age, position) VALUES (?,?,?)', ['david', 42, 'baker']);
@@ -31,17 +26,20 @@ const menu = () => {
     console.log('4.Delete employees');
     console.log('5.EXIT');
     const userInput = prompt('Enter your response: ');
-    return userInput;
+    const input = parseInt(userInput);
+    console.clear()
+    return input;
 }
 
-/*  console.log('Welcome to CRM!');
+console.log('Welcome to CRM!');
 console.log('1.View employees');
 console.log('2.Create employee');
 console.log('3.Update employee');
 console.log('4.Delete employees');
 console.log('5.EXIT');
 const userInput = prompt('Enter your response: ');
-const input = parseInt(userInput);  */
+const input = parseInt(userInput);
+
 
 const view = () => {
     connection.query('SELECT * FROM employees', function (err, result) {
@@ -50,8 +48,6 @@ const view = () => {
     })
     connection.commit();
 }
-
-
 const create = () => {
     const name = prompt('What is the employee name?');
     const age = prompt('What is the employees age? ');
@@ -69,7 +65,7 @@ const update = () => {
     const newAge = prompt('What is the employees new age?');
     const newPosition = prompt('What is the employees new position?');
     connection.query('UPDATE employees SET name = ?, age = ?, position = ? WHERE id = ? ', [newName, parseInt(newAge), newPosition, parseInt(employeeId)], function (err, result) {
-        if (err) throw (err);
+        if (err) console.log(err);
         console.log(result);
     })
     connection.commit();
@@ -84,30 +80,18 @@ const deleteEmp = () => {
 
 
 
-
-
-
-connection.connect();
-
-
-let userInput: string;
-do {
-    userInput = menu();
-    if (userInput === '1') view();
-    if (userInput === '2') create();
-    if (userInput === '3') update();
-    if (userInput === '4') deleteEmp();
-} while (userInput !== '5');
- 
-
+while (true) {
+    if (input === 1) view();
+    if (input === 2) create();
+    if (input === 3) update();
+    if (input === 4) deleteEmp();
+    if (input > 4) break
+    
+    break
+}
 
 
 connection.end();
-
-
-
-
-
 
 
 
